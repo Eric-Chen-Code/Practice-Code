2c1eb05e766522ace85e29e948a31d0637941375
ericdachen <erche3742@gmail.com>
2020-06-12 13:16:10 -0400

    Updating more website

--- JavaScript_Code/4-DOM-pig-game/starter/app.js
+++ JavaScript_Code/4-DOM-pig-game/starter/app.js
@@ -9,13 +9,41 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer; 
-scores = [0,0];
+var scores, roundScore, activePlayer;
+scores = [0, 0];
 roundScore = 0;
 activePlayer = 1;
 
-dice = Math.floor(Math.random() * 6) + 1;
-console.log(dice);
+//console.log(dice);
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector("#current-" + activePlayer).textContent = dice;
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+//var x = document.querySelector("#score-0").textContent;
+//console.log(x);
+
+document.querySelector(".dice").style.display = "none";
+
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
+function btn() {
+  //do something here
+}
+
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var dice = Math.floor(Math.random() * 6) + 1;
+  var diceDom = document.querySelector(".dice");
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + dice + ".png";
+
+  if (dice > 1) {
+    // add score
+  } else {
+    //next player
+  }
+  // Can also write function here directly into event listener method
+});
+//btn function is called call back function because it is not called manually
