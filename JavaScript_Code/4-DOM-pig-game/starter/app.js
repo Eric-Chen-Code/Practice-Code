6c87d4798e9b969ad32dcdea7f59385da8428af8
ericdachen <erche3742@gmail.com>
2020-06-20 18:34:40 -0400

    Adding hold button

--- JavaScript_Code/4-DOM-pig-game/starter/app.js
+++ JavaScript_Code/4-DOM-pig-game/starter/app.js
@@ -10,9 +10,27 @@ GAME RULES:
 */
 
 var scores, roundScore, activePlayer;
-scores = [0, 0]; //maybe
-roundScore = 0;
-activePlayer = 0;
+
+function init() {
+  scores = [0, 0];
+  roundScore = 0;
+  activePlayer = 0;
+
+  document.querySelector(".dice").style.display = "none";
+  document.getElementById("score-0").textContent = "0";
+  document.getElementById("score-1").textContent = "0";
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
+  document.getElementById("name-0").textContent = "Player 1";
+  document.getElementById("name-1").textContent = "Player 2";
+  document.querySelector(".player-0-panel").classList.remove("winner");
+  document.querySelector(".player-1-panel").classList.remove("winner");
+  document.querySelector(".player-0-panel").classList.remove("active");
+  document.querySelector(".player-1-panel").classList.remove("active");
+  document.querySelector(".player-0-panel").classList.add("active");
+}
+
+init();
 
 //console.log(dice);
 
@@ -21,13 +39,6 @@ activePlayer = 0;
 
 //var x = document.querySelector("#score-0").textContent;
 //console.log(x);
-
-document.querySelector(".dice").style.display = "none";
-
-document.getElementById("score-0").textContent = "0";
-document.getElementById("score-1").textContent = "0";
-document.getElementById("current-0").textContent = "0";
-document.getElementById("current-1").textContent = "0";
 
 function btn() {
   //do something here
@@ -65,10 +76,18 @@ document.querySelector(".btn-hold").addEventListener("click", function () {
     scores[activePlayer];
 
   // Check if player won the game
-  scores[activePlayer] >= 100 ? console.log("Player " + activePlayer " wins!")
-
-  // Next player
-  nextPlayer();
+  if (scores[activePlayer] >= 100) {
+    document.querySelector("#name-" + activePlayer).textContent = "Winner";
+    document.querySelector(".dice").style.display = "none";
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.add("winner");
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.remove("active");
+  } else {
+    nextPlayer();
+  }
 });
 //btn function is called call back function because it is not called manually
 
@@ -84,6 +103,7 @@ function nextPlayer() {
 
   document.querySelector(".player-0-panel").classList.toggle("active");
   document.querySelector(".player-1-panel").classList.toggle("active");
-
   document.querySelector(".dice").style.display = "none";
 }
+
+document.querySelector(".btn-new").addEventListener("click", init);
