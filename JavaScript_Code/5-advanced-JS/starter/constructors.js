cebee173605d2bfcde70fa128e3b877a66ab1ee3
ericdachen <erche3742@gmail.com>
2020-07-13 14:35:13 -0400

    Adding constructor functions

--- JavaScript_Code/5-advanced-JS/starter/constructors.js
+++ JavaScript_Code/5-advanced-JS/starter/constructors.js
@@ -6,22 +6,38 @@ var john = {
   job: "teacher",
 };
 
+//Function constructors always have capitals
+
 var Person = function (name, yearOfBirth, job) {
   this.name = name;
   this.yearOfBirth = yearOfBirth;
   this.job = job;
-  this.calculateAge = function () {
-    console.log(2016 - this.yearOfBirth);
-  };
 };
 
 //new creates a new empty object
-var john = new Person("John", 1990, "teacher");
 
-john.calculateAge();
+Person.prototype.calculateAge = function () {
+  console.log(2016 - this.yearOfBirth);
+};
+
+Person.prototype.lastName = "Smith";
+
+var john = new Person();
+john.name = "John";
+john.yearOfBirth = 1990;
+john.job = "teacher";
+console.log(john);
 
 var jane = new Person("Jane", 1969, "designer");
 jane.calculateAge;
 
 var mark = new Person("Mark", 1948, "retired");
 mark.calculateAge;
+
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
+
+console.log(john.lastName);
+console.log(jane.lastName);
+console.log(mark.lastName);
