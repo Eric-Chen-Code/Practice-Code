dc5c00289cbb17079f2edec1f0212a488f47980e
Eric Chen <e56chen@uwaterloo.ca>
2021-05-21 23:23:42 -0400

    Change

---
+++ C++/change-counter.cc
@@ -0,0 +1,24 @@
+#include <cstddef>
+#include <string.h>
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+double change = 1.00; //YO I'm not sure how u get the input but just put it here
+int dollars = 0;
+int quarters = 0;
+int dimes = 0;
+int nickels = 0;
+int pennies = 0;
+
+if (change == 0)
+{
+    return "No change";
+}
+else
+{
+    while (change != 0)
+    {
+    }
+}
